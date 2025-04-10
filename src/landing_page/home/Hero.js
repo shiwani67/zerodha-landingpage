1f52172f9f597129176444388cbeeb6121256a8f
shiwani_gupta99 <142133767+shiwani67@users.noreply.github.com>
2025-04-10 16:52:45 +0530

    lil change

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
        <div className='container p-5 mb-5'> 
            <div className='row text-center'>
-               <img src='media/homehero.png' alt='Hero Image' className='mb-5'/>
+               <img src='media/homeHero.png' alt='Hero Image' className='mb-5'/>
                <h1 className='mt-5'>Invest In Everything</h1> 
                <p>Online Platform to invest in stocks, derivatives, mutual funds, and more</p>
                <div className='d-flex justify-content-center'>
